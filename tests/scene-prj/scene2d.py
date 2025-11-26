a7a1ab97c1528a2f1b2148257bcfdcf5c2399f1f
guillaumeLozenguez <guillaume@drods.net>
2025-11-26 14:53:36 +0100

    wip

---
+++ tests/scene-prj/scene2d.py
@@ -0,0 +1,5 @@
+class Camera :
+    def __init__(self):
+        self._scale= 10
+        self._position_x= 0.0
+        self._position_y= 0.0
---
+++ tests/scene-prj/test_01_package.py
@@ -0,0 +1,10 @@
+# try to import the package:
+def test_import():
+    import scene2d
+
+# import the package one for all :
+import scene2d
+
+# try to instanciate a camera :
+def test_camera():
+    camera= scene2d.Camera()
